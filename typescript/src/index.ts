b8d54dd854ce4ab3dae3e809d5fd95ff763c270f
Yota Toyama <raviqqe@gmail.com>
2018-08-15 22:48:37 +0000

    Write script

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,1 +1,20 @@
+import { driver, structure } from "gremlin";
 
+const g = new structure.Graph()
+  .traversal()
+  .withRemote(new driver.DriverRemoteConnection("ws://localhost:8182/gremlin"));
+
+setInterval(async () => {
+  try {
+    console.log(
+      await g
+        .V()
+        .has("name", "jupiter")
+        .out("brother")
+        .values("name")
+        .toList()
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}, 60000);
